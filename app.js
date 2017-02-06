8ccff01ee0c525254d38fc340bebf0e3c1257ff0
JNeel <neeljesse@gmail.com>
2017-02-05 22:11:43 -0800

    mongoose, modules

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 var express = require("express");
 var morgan = require('morgan');
+var mongoose = require('mongoose');
 
 var app = express();
 
-//Middleware
+mongoose.connect('mongodb://root:qqqqqq@ds011902.mlab.com:11902/ecommerce', function(err){
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("Connected to the database");
+    }
+});
 
+//Middleware
 app.use(morgan('dev'));
 
 
