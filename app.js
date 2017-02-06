ca3889933edf2340dfdda3e1fc0843d69f4b9487
JNeel <neeljesse@gmail.com>
2017-02-05 19:27:13 -0800

    Morgan

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 var express = require("express");
+var morgan = require('morgan');
 
 var app = express();
+
+//Middleware
+
+app.use(morgan('dev'));
+
+
+app.get('/', function(req, res){
+    var name = "Jesse";
+    res.json("My name is " + name);
+    
+});
+
+app.get('/catname', function(req, res){
+    var name = "Jesse";
+    res.json("My name is " + name);
+    
+});
+
+
+//app.post()
+
+//app.put()
+
+//app.delete()
 
 app.listen(3000, function(err){
     if (err) throw err;
